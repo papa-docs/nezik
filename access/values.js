a930e6a065d0257118c23688e99eb3011362c93a
papa-docs <papa-docs1@proton.me>
2025-05-14 22:30:45 +0300

    Update values.js

--- access/values.js
+++ access/values.js
@@ -4,21 +4,21 @@ var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
 var birthdate = "15.04.2000"; // Дата рождения
-var fullname = "Метлiн Давид Арсенович" // ФИО
-var name = "Давид" // Имя
+var fullname = "Маковецький Дамiр Констянтинович" // ФИО
+var name = "Дамiр" // Имя
 
 // Паспорт
-var passport_id = "003519034" // Номер паспорта
+var passport_id = "003519022" // Номер паспорта
 
 // КПП
-var kpp_id = "3872011238" // ИНН
+var kpp_id = "3823461956" // ИНН
 
 // Права
-var rights_valid_until = "29.11.2051" // Права "Дійсні до"
+var rights_valid_until = "08.04.2041" // Права "Дійсні до"
 var rights_categories = "A, B" // Права "Категорії"
-var rights_tsc = "ТСЦ 1242" // Права "Видав
-var rights_id = "BXM826411" // Права "Серія та номер""
+var rights_tsc = "ТСЦ 2642" // Права "Видав
+var rights_id = "KKM235413" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "09.08.2023" // Сертификат "Дійсний до"
+var covid_valid_until = "09.08.2024" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
